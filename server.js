ca54fa8094853ba91562d860be80f59490d70a6b
RodrigoH95 <HRodrigo95@hotmail.com>
2023-01-11 03:04:16 -0300

    added express

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const io = require("socket.io")(3000, {
+const express = require('express');
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
   cors: {
     origin: ['https://mensajea.netlify.app/']
   }
@@ -8,3 +11,7 @@ io.on("connection", socket => {
   console.log(socket.id);
   socket.on("send-message", (message) => socket.broadcast.emit("receive-message", message));
 });
+
+server.listen(process.env.PORT, () => {
+  console.log("Server running on port" + process.env.PORT);
+})
